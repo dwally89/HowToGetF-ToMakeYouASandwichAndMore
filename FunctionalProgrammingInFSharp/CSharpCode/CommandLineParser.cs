cc01290c2b8cb574413df5f28ccb4cb010d2401e
Daniel Wallis <dwally89@gmail.com>
2018-04-15 21:36:01 -0400

    Split out more slides

---
+++ FunctionalProgrammingInFSharp/CSharpCode/CommandLineParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Parser
+{
+    public class CommandLineParser
+    {
+        public static T GetArgument<T>(string prompt, Func<string, T> parser)
+        {
+            Console.Write(prompt);
+            return parser(Console.ReadLine());
+        }
+
+        public static int GetInteger()
+        {
+            return GetArgument("Enter an integer: ", int.Parse);
+        }
+    }
+}
