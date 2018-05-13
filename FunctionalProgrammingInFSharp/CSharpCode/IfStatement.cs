c5b7d24ea16a91aba5abae5bd18bfd1bb16c5330
Daniel Wallis <dwally89@gmail.com>
2018-05-13 17:32:16 -0400

    Add another slide for if expressions

---
+++ FunctionalProgrammingInFSharp/CSharpCode/IfStatement.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System;
+using System.Linq;
+
+public class IfStatement
+{
+    public static bool IsInTheBeatles(string name) => 
+        new[] { "John", "Paul", "George", "Ringo" }.Contains(name);
+
+    public static bool MoreComplicatedFunction(string name) => false;
+
+    public static void AnalyseDrummer(string drummerName)
+    {
+        bool isTheGreatest;
+        if (drummerName == "Ringo")
+        {
+            if (IsInTheBeatles(drummerName))
+            {
+                isTheGreatest = false;
+            }
+            else
+            {
+                isTheGreatest = MoreComplicatedFunction(drummerName);
+            }
+        }
+        else
+        {
+            isTheGreatest = MoreComplicatedFunction(drummerName);
+        }
+
+        if (isTheGreatest)
+        {
+            Console.WriteLine($"{drummerName} is the greatest drummer");
+        }
+    }
+}
